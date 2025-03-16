369311df9612f46fab4271d848549146c2108849
Meet Thummar <mrt00998@gmail.com>
2025-03-16 17:48:49 +0300

    Day 1 changes

---
+++ Day - 1/main.py
@@ -0,0 +1,12 @@
+print("Meet")
+
+print("Your name is " + "Meet")
+
+name = input("what is your name? \n")
+print(len(name))
+
+sch_name = input("what is your school name? \n")
+pet_name = input("what is your pet name? \n")
+
+final_name = "Your band name is " + sch_name + " " + pet_name
+print(final_name)
