4ec67fb5fac8c33c300dd3faa691133dd607da57
iMeetMinds <imeetminds@gmail.com>
2025-03-29 09:01:58 +0300

    Day 20/21 Colding challenges

---
+++ Day - 20/01_snakeGame.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor('black')
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.food_eaten()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        is_game_on = False
+        scoreboard.game_over()
+
+    for seg in snake.all_segments[1:]:
+        if snake.head.distance(seg) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
