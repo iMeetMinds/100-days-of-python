2d12df7a931e2cc9b2efebcc8b0cc36f94823b0c
iMeetMinds <imeetminds@gmail.com>
2025-03-29 09:09:45 +0300

    Color changes

--- Day - 20/food.py
+++ Day - 20/food.py
@@ -1,18 +1,20 @@
 from turtle import Turtle
 import random
 
+COLORS = ['red','yellow','orange','blue','green','purple']
+
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape('circle')
+        self.shape('turtle')
         self.penup()
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
-        self.color('blue')
         self.speed('fastest')
         self.refresh()
 
     def refresh(self):
+        self.color(random.choice(COLORS))
         ran_x = random.randint(-280, 280)
         ran_y = random.randint(-280, 280)
         self.goto(ran_x, ran_y)
