302b88848589c7db4e7ceebb7d840b8bafec7582
iMeetMinds <imeetminds@gmail.com>
2025-03-30 22:00:05 +0300

    Highscore file added with code changes

--- Day - 20/01_snakeGame.py
+++ Day - 20/01_snakeGame.py
@@ -34,12 +34,14 @@ while is_game_on:
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
         is_game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for seg in snake.all_segments[1:]:
         if snake.head.distance(seg) < 10:
             is_game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- Day - 20/scoreboard.py
+++ Day - 20/scoreboard.py
@@ -1,21 +1,40 @@
 from turtle import Turtle
 
+FONT = ('Arial', 12, 'bold')
+
+def fetch_highscore():
+    with open("high_score.txt") as file:
+        return int(file.read())
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = -1
+        self.high_score = fetch_highscore()
         self.penup()
         self.goto(-20,280)
         self.color('white')
         self.food_eaten()
         self.hideturtle()
+        self.update_scoreboard()
 
     def food_eaten(self):
-        self.clear()
         self.score += 1
-        self.write(f"Score : {self.score}", False, align='center', font=('Arial', 12, 'bold'))
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", False, align='center', font=('Arial', 12, 'bold'))
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score : {self.score}, High Score : {self.high_score}", False, align='center', font=FONT)
+        
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER!", False, align='center', font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode='w') as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
