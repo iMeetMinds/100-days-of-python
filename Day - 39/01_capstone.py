d5048fa72ccfecb666cb9eb307ebb28b8b81d8da
iMeetMinds <imeetminds@gmail.com>
2025-04-11 22:58:20 +0300

    Day 40 coding challenges

--- Day - 39/01_capstone.py
+++ Day - 39/01_capstone.py
@@ -5,16 +5,27 @@ from notification_manager import NotificationManager
 flight_search = FlightSearch()
 flight_data = FlightData()
 
-destination_place = input("Where You want to go : ")
-destination_code = flight_search.fetch_iata(destination_place)
+is_journey_continued = True
 
+while is_journey_continued:
+    destination_place = input("Where You want to go : ")
+    destination_code = flight_search.fetch_iata_v1(destination_place)
 
+    if destination_code:
+        flight_fetch_data = flight_search.search_flights(destination_code)
 
-flight_fetch_data = flight_search.search_flights(destination_code)
+        lowest_flight_data = flight_data.fetch_lowest_flight_data(flight_fetch_data)
 
-lowest_flight_data = flight_data.fetch_lowest_flight_data(flight_fetch_data)
+        if lowest_flight_data and len(lowest_flight_data) > 0:
+            location_dict = flight_fetch_data['dictionaries']['locations']
+            noti_message = flight_data.prepare_noti_msg(lowest_flight_data[0], destination_place, location_dict, flight_search)
+            send_noti = NotificationManager()
+            send_noti.send_mail(noti_message)
 
-if len(lowest_flight_data) > 0:
-    send_noti = NotificationManager()
-    send_noti.send_mail(lowest_flight_data[0], destination_place)
-    print("Mail Send Successfully!!")
+            print("Mail Send Successfully!!")
+            is_journey_continued = False
+        else:
+            print("No cheap flights available as of now. Kindly check later!!")
+            is_journey_continued = False
+    else:
+        print("Invalid Destination. Kindly provide proper destination.")
--- Day - 39/flight_search.py
+++ Day - 39/flight_search.py
@@ -59,8 +59,29 @@ class FlightSearch:
         }
 
         city_data = {
-            'subType': 'CITY',
-            'keyword': city_name
+            'keyword' : f"{city_name}",
+            'subType' : 'AIRPORT'
+        }
+
+
+        response = requests.get(url=const.AMADEUS_FETCH_IATA_API_AIRPORT, headers=city_headers,
+                                params=city_data)
+        response.raise_for_status()
+        fetched_data = response.json()['data']
+        for n_dict in fetched_data:
+            return n_dict['iataCode']
+
+        return None
+
+    def fetch_iata_v1(self, city_name):
+        city_headers = {
+            'Authorization': self.bearer_token,
+        }
+
+        city_data = {
+            'keyword' : city_name,
+            'max' : 10,
+            'include' : 'AIRPORTS'
         }
 
         try:
@@ -69,11 +90,44 @@ class FlightSearch:
             response.raise_for_status()
             fetched_data = response.json()['data']
             for n_dict in fetched_data:
-                if n_dict['name'].lower() == city_name.lower():
+                if n_dict['name'].lower() == city_name.lower() and n_dict['subType'] == 'city':
                     return n_dict['iataCode']
 
             return None
         except requests.exceptions.RequestException:
             self.login_token()
-            self.fetch_iata(city_name)
+            self.fetch_iata_v1(city_name)
+
+    def fetch_city_name(self, iata_code, country_code):
+        city_headers = {
+            'Authorization': self.bearer_token,
+        }
+
+        city_data = {
+            'countryCode' : country_code,
+            'keyword' : iata_code,
+            'max' : 10,
+            'include' : 'AIRPORTS'
+        }
+        print(f"city data=> {city_data}")
+        try:
+            response = requests.get(url=const.AMADEUS_FETCH_IATA_API, headers=city_headers,
+                                    params=city_data)
+            response.raise_for_status()
+            fetched_data = response.json()['data']
+            for n_dict in fetched_data:
+                if n_dict['iataCode'] and n_dict['address']['countryCode'] == country_code:
+                    return n_dict['name']
+
+            return None
+        except requests.exceptions.RequestException:
+            self.login_token()
+            self.fetch_city_name(iata_code, country_code)
+
+
+    def fetch_country_name(self, country_code):
+        response = requests.get(url=f"{const.COUNTRY_FETCH_API}{country_code}")
+        response.raise_for_status()
+        fetched_data = response.json()[0]
+        return fetched_data['name']['common']
 
